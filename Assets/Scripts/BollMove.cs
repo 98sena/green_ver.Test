2257b2c6fbea13094ac49c71afb0f7e2f12143d5
Kim Yeri <98sena@naver.com>
2020-07-14 23:25:55 +0900

    init 0714

---
+++ Assets/Scripts/BollMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class BollMove : MonoBehaviour
+{
+    public GetPower gp;
+    Rigidbody rb;
+    Vector3 target = new Vector3(23.4f, 6.25f, 29.49f);
+
+    public PracticeMode pm;
+    public bool succeed = false;
+    Vector3 newPos;
+    Vector3 beforePos = new Vector3(23.4f, 6.25f, 29.49f); //
+
+    int cnt = 0;
+    float fraction = 0.7f;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        rb = transform.gameObject.GetComponent<Rigidbody>();
+        newPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (pm.state == Progress.StateLevel.Roll)
+        {
+            if (gp.power <= 1)
+            {
+                gp.getInput = 3;
+                if (Mathf.Abs(target.x-transform.position.x)<2.0f&& Mathf.Abs(target.z - transform.position.z) < 2.0f)
+                {
+                    succeed = true;
+                }
+                return;
+            }
+            cnt++;
+            //Debug.Log(cnt+"       "+Time.deltaTime);
+
+            newPos.z = transform.position.z + Time.deltaTime * gp.power;
+            //transform.position = newPos;
+            gp.power = gp.power - Time.deltaTime * fraction * gp.power;
+
+            //transform.position = Vector3.Lerp(transform.position, new Vector3(23.8f,3.74f,30f), 0.1f * Time.deltaTime);
+            //gp.power--;
+
+            transform.position = Vector3.Lerp(transform.position, beforePos, 0.1f * Time.deltaTime);
+            //beforePos = newPos;
+
+        }
+    }
+}
---
+++ Assets/Scripts/PracticeMode.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PracticeMode : Progress
+{
+    public GameObject selectButtons;
+    public GameObject startButtons;
+    public GameObject readyButtons;
+    public GetPower gp;
+    
+    public BollMove bm;
+
+    public GameObject endButtons;
+    public GameObject againUI;
+
+    public int hitCount = 0;
+    int practiceCount;
+
+    public Text hitCntObj;
+    public Text practiceCntObj;
+
+    public GameObject ball;
+    public GameObject startPos;
+    public GameObject cameraPos;
+    public GameObject startCameraPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = 0;
+        practiceCount = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hitCntObj.text = "" + hitCount;
+        practiceCntObj.text = "" + (practiceCount + 1);
+        run(state);
+    }
+
+    public override void startState()
+    {
+        if (!startButtons.activeSelf)
+        {
+            startButtons.SetActive(true);
+        }
+        if (againUI.activeSelf)
+        {
+            StartCoroutine(WaitForNotice());
+        }
+        if (endButtons.activeSelf)
+        {
+            endButtons.SetActive(false);
+        }
+        Debug.Log("START STATE");
+    }
+    public void gotoReady()
+    {
+        if (selectButtons.activeSelf)
+        {
+            selectButtons.SetActive(false);
+        }
+        nextState();
+    }
+    IEnumerator WaitForNotice()
+    {
+        yield return new WaitForSeconds(1.0f);
+        againUI.SetActive(false);
+    }
+    public override void readyState()
+    {
+        
+        if (!readyButtons.activeSelf)
+        {
+            Debug.Log("Hi Start");
+            readyButtons.SetActive(true);
+        }
+        if (startButtons.activeSelf)
+        {
+            Debug.Log("bye Start");
+            startButtons.SetActive(false);
+        }
+        
+        Debug.Log("ready");
+        if (gp.getInput == 2)
+        {
+            nextState();
+        }
+    }
+    public override void rollState()
+    {
+        if (gp.getInput == 3)
+        {
+            Debug.Log("BYE ROLL");
+            gp.getInput = 0;
+            nextState();
+            if (readyButtons.activeSelf)
+            {
+                readyButtons.SetActive(false);
+            }
+        }
+    }
+    public override void endState()
+    {
+        Debug.Log("END STATE");
+        if (bm.succeed)
+        {
+            endButtons.SetActive(true);
+        }
+        else
+        {
+            if (!endButtons.activeSelf)
+            {
+                againUI.SetActive(true);
+            }
+            nextState();
+        }
+    }
+
+    public void oneMore()
+    {
+        practiceCount++;
+        hitCount = 0;
+        nextState();
+        if (!selectButtons.activeSelf)
+        {
+            selectButtons.SetActive(true);
+        }
+        ball.transform.position = startPos.transform.position;
+        cameraPos.transform.position = startCameraPos.transform.position;
+        bm.succeed = false;
+    }
+}
